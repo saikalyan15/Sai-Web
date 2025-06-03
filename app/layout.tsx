18159badabb864711dc4c3e28587d0d70b624da3
Sai <saikalyan.akunuri@gmail.com>
2025-06-03 21:33:58 +0530

    Brand new website using NextJS

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type React from "react"
+import "@/app/globals.css"
+import { Inter } from "next/font/google"
+import { ThemeProvider } from "@/components/theme-provider"
+
+const inter = Inter({ subsets: ["latin"] })
+
+export const metadata = {
+  title: "Saikalyan Akunuri - n8n Automation Consultant",
+  description:
+    "Helping you automate smarter with n8n. I design open-source automations that save time and reduce costs.",
+    generator: 'v0.dev'
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
--- archetypes/posts.md
+++
@@ -1,6 +0,0 @@
----
-date: '{{ now.Format "Monday, January 2, 2006"}}'
-title: '{{ .File.TranslationBaseName | replaceRE "-" " " | title }}'
-description: "This is a short description of the page"
-draft: true
----
