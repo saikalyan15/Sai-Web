e37b8f546f3def0ea88fc7a3f554795bf636f075
Sai <saikalyan.akunuri@gmail.com>
2025-06-04 22:53:06 +0530

    changes to footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -4,32 +4,26 @@ export function Footer() {
   return (
     <footer className="text-sm text-gray-700 py-6 text-center border-t mt-12">
       <div className="max-w-7xl mx-auto px-4">
-        <div className="mb-6">
+        <div className="mb-8">
           <nav className="space-x-4">
-            <Link href="/" className="hover:text-gray-900 hover:underline">
+            <Link href="/" className="hover:underline">
               Home
             </Link>
-            <Link href="/about" className="hover:text-gray-900 hover:underline">
+            <Link href="/about" className="hover:underline">
               About
             </Link>
-            <Link
-              href="/services"
-              className="hover:text-gray-900 hover:underline"
-            >
+            <Link href="/services" className="hover:underline">
               Services
             </Link>
-            <Link href="/blog" className="hover:text-gray-900 hover:underline">
+            <Link href="/blog" className="hover:underline">
               Blog
             </Link>
-            <Link
-              href="/contact"
-              className="hover:text-gray-900 hover:underline"
-            >
+            <Link href="/contact" className="hover:underline">
               Contact
             </Link>
           </nav>
         </div>
-        <p className="text-gray-500">
+        <p className="text-muted-foreground text-sm">
           © 2025 Saikalyan Akunuri. All rights reserved.
         </p>
       </div>
